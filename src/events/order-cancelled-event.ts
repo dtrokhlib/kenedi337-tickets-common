22029cd16e514bc349b6e8f29ae8e931625ab73c
Dmitry Trokhlib <fgh98zip@gmail.com>
2022-02-17 19:47:43 +0200

    Updates

--- src/events/order-cancelled-event.ts
+++ src/events/order-cancelled-event.ts
@@ -3,10 +3,9 @@ import { Subjects } from './subjects';
 
 export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
-  data: {
     id: string;
     ticket: {
       id: string;
     };
-  };
+
 }
